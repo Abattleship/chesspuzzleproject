892a43e48419a189e11e5fe4d169377211213773
Abattleship <mikebarnes91@gmail.com>
2022-11-15 22:21:33 -0700

    chess project changes

---
+++ script.js
@@ -0,0 +1,23 @@
+let chessPieces = ["♟︎", "♘", "♟︎", "♘", "♟︎", "♝", "♟︎", "♝", "♜", "♟︎", "♟︎", "♜", "♕", "♔", "♟︎", "♟︎"]
+let pawnShelf = document.getElementById("pawn-shelf")
+let minorPiecesShelf = document.getElementById("minorpieces-shelf")
+let rookShelf = document.getElementById("rook-shelf")
+let royalShelf = document.getElementById("royal-shelf")
+
+function sortPieces() { for (let i = 0; i < chessPieces.length; i++) {
+    if (chessPieces[i] === "♟︎") {
+        pawnShelf.textContent += "♟︎"
+    }  else if (chessPieces[i] === "♘") {
+        minorPiecesShelf.textContent += "♘"
+    } else if (chessPieces[i] === "♝") {
+        minorPiecesShelf.textContent += "♝"
+    } else if (chessPieces[i] === "♜") {
+        rookShelf.textContent +=  "♜"
+    } else if (chessPieces[i] === "♕") {
+        royalShelf.textContent += "♕"
+    } else if (chessPieces[i] === "♔") {
+        royalShelf.textContent += "♔"
+    }
+}
+  
+}sortPieces()
